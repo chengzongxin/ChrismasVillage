35cbd1f20e71440e8293c83f8a8b0455dce5cb4d
USER-20210815FL\Administrator <8876142@qq.com>
2022-01-22 23:02:33 +0800

    富文本

--- scripts/global/global.lua
+++ scripts/global/global.lua
@@ -7,20 +7,17 @@ local function playerGroup()
     local players = {}
     for i = 0, 12 do
         local p = cj.Player(i)
-        
-        -- if ((GetPlayerController(Player(0)) == MAP_CONTROL_USER)) then
-        -- if ((GetPlayerSlotState(Player(0)) == PLAYER_SLOT_STATE_PLAYING)) then
         -- 玩家的控制者是用户，并且游戏状态是正在游戏
-        -- print(cj.GetPlayerName(p).." Player Controller is : "..cj.GetPlayerController(p).."  slot state : "..cj.GetPlayerSlotState(p))
         if cj.GetPlayerController(p) == cj.MAP_CONTROL_USER and cj.GetPlayerSlotState(p) == cj.PLAYER_SLOT_STATE_PLAYING then
-            -- print(cj.GetPlayerName(p).."is user")
+            print(cj.GetPlayerName(p).." Player Controller is : "..cj.GetPlayerController(p).."  slot state : "..cj.GetPlayerSlotState(p))
             table.insert(PlayerGroup, p)
         end
     end
     -- 查询完成
     print("print all players,count = "..#PlayerGroup)
     for key, value in pairs(PlayerGroup) do
-        print(value)
+        local countStr = string.format( " |cffffff00%d|r ", #PlayerGroup)
+        echo("本局游戏玩家数量为"..countStr.."人",value,0,0)
     end
 end
 
