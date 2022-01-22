a5e2177b64129a5fd6fa5ab6f6c2722707cfb215
USER-20210815FL\Administrator <8876142@qq.com>
2022-01-23 02:40:29 +0800

    选择英雄

--- scripts/game/select_hero.lua
+++ scripts/game/select_hero.lua
@@ -1,3 +1,38 @@
+
+local function select_unit (u)
+    local trg = cj.CreateTrigger()
+    cj.TriggerRegisterUnitEvent(trg, u, cj.EVENT_UNIT_SPELL_CAST)
+    -- cj.TriggerRegisterAnyUnitEventBJ(trg,cj.EVENT_PLAYER_UNIT_SPELL_CAST)
+    cj.TriggerAddAction(trg,function ()
+        -- 釋放了技能
+        local abi_id = cj.GetSpellAbilityId()
+        echo("釋放了技能.."..abi_id)
+        local u = cj.GetTriggerUnit()
+        local p = cj.GetOwningPlayer(u)
+        local x = cj.GetUnitX(u)
+        local y = cj.GetUnitY(u)
+        if abi_id == str2id('A005:ANcl') then
+            echo("釋放了技能..选择步兵")
+            cj.CreateUnit(p,str2id('H003:Hpal'),x,y,0)
+        end
+        if abi_id == str2id('A007:ANcl') then
+            echo("釋放了技能..选择弓箭手")
+            cj.CreateUnit(p,str2id('H004:Hpal'),x,y,0)
+        end
+        if abi_id == str2id('A006:ANcl') then
+            echo("釋放了技能..选择火枪手")
+            cj.CreateUnit(p,str2id('H002:Hpal'),x,y,0)
+        end
+        if abi_id == str2id('A003:ANcl') then
+            echo("釋放了技能..选择牛头人")
+            cj.CreateUnit(p,str2id('H000:Hpal'),x,y,0)
+        end
+
+        cj.RemoveUnit(u)
+    end)
+
+end
+
 function create_select_unit()
     echo("|cffff0000请|r|cffdb2400选|r|cffb74800择|r|cff926d00你|r|cff6e9100的|r|cff49b600英|r|cff25da00雄|r|cff00ff00！|r",nil,30,0,0)
     for _, player in pairs(PlayerGroup) do
@@ -6,6 +41,8 @@ function create_select_unit()
         local x = -300
         local y = 0
         local u = cj.CreateUnit(player,id, x, y, 0)
-        -- cj.IssuePointOrder(u, "attack", 0, 0)
+        -- 平移镜头
+        cj.PanCameraToTimed(cj.GetUnitX(u), cj.GetUnitY(u), 0)
+        select_unit(u)
     end
 end
--- scripts/kit/kit.lua
+++ scripts/kit/kit.lua
@@ -1,5 +1,5 @@
 cj = require "jass.common"
-
+-- bj = require "blizzard"
 -- 物编转id
 function str2id(a)
     local n1 = string.byte(a, 1) or 0
@@ -9,5 +9,12 @@ function str2id(a)
     local r = n1*256*256*256+n2*256*256+n3*256+n4
     return r
 end
+
+-- function PanCameraToTimedLocForPlayer takes player whichPlayer, location loc, real duration returns nothing
+--     if (GetLocalPlayer() == whichPlayer) then
+--         // Use only local code (no net traffic) within this block to avoid desyncs.
+--         call PanCameraToTimed(GetLocationX(loc), GetLocationY(loc), duration)
+--     endif
+-- endfunction
 
 require 'kit.echo'
