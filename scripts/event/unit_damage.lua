4374f9490df11c5b75e3cfde1e61a46df151efc7
USER-20210815FL\Administrator <8876142@qq.com>
2022-01-27 00:26:03 +0800

    添加伤害字幕

--- scripts/event/unit_damage.lua
+++ scripts/event/unit_damage.lua
@@ -6,6 +6,15 @@ local function unit_damage()
         local dmg = cj.GetEventDamage()
 
         echo(uname(us).." attck "..uname(ut).." damage = "..dmg)
+        local x = cj.GetUnitX(ut)
+        local y = cj.GetUnitY(ut)
+        local t = cj.CreateTextTag()
+        cj.SetTextTagPos(t,x,y,0)
+        cj.SetTextTagColor(t,255,255,0,255)
+        cj.SetTextTagText(t,dmg,15)
+        time_wait(3,function ()
+            cj.DestroyTextTag(t)
+        end)
     end)
 end
 
