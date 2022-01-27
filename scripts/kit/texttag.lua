5f9f76748ce85d4c9a6cb87ef52c7adc90b4ccde
USER-20210815FL\Administrator <8876142@qq.com>
2022-01-27 23:34:38 +0800

    beifen

--- scripts/kit/texttag.lua
+++ scripts/kit/texttag.lua
@@ -2,13 +2,27 @@ function TextTag(text,x,y,height,size)
     local t = cj.CreateTextTag()
     cj.SetTextTagPos(t,x,y,height)
     cj.SetTextTagColor(t,255,255,255,255)
-    cj.SetTextTagText(t,text,size * 0.023 / 10)
+    cj.SetTextTagText(t,text,TextTagSize(size))
     cj.SetTextTagVisibility(t, true)
-    local xv = random(-0.6, 0.6)
+    local xv = math.random(5) / 10.0
     local yv = (1 - xv^2)^0.5
-    cj.SetTextTagVelocity(t,xv,yv)
-    time_wait(3,function ()
-        cj.DestroyTextTag(t)
+    cj.SetTextTagVelocity(t,xv*0.2,yv*0.2)
+    local times = 0
+    local timer = cj.CreateTimer()
+    cj.TimerStart(timer, 0.03, true, function ()
+        cj.SetTextTagText(t,text,TextTagSize(size+10-times))
+        -- 1秒后销毁
+        times = times + 1
+        if times > 11 then
+            cj.DestroyTimer(timer)
+            cj.DestroyTextTag(t)
+        end
+
     end)
+
     return t
+end
+
+function TextTagSize(size)
+    return size * 0.023 / 10
 end
