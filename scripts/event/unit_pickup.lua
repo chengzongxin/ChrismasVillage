dfbc209b8c3c09e369a58cb862116d4af9160067
USER-20210815FL\Administrator <8876142@qq.com>
2022-01-29 00:14:28 +0800

    合成系统搞定

--- scripts/event/unit_pickup.lua
+++ scripts/event/unit_pickup.lua
@@ -1,3 +1,4 @@
+
 local function unit_pickup()
     cevent.unit_pickup(function ()
         local unit = cj.GetTriggerUnit()
@@ -6,23 +7,24 @@ local function unit_pickup()
         -- 物品叠加
         
         local item_id = cj.GetItemTypeId(item)
-        local jiejin_id = str2id('I008:engs')
         
-        if item_id == jiejin_id then
+        if item_id == JIEJIN_ID then
             -- 获得结晶
             for i = 0, 5 do
                 local it = cj.UnitItemInSlot(unit,i)
                 local it_id = cj.GetItemTypeId(it)
                 -- 获取的和背包的当前遍历的不是一个物品，并且是结晶，则叠加物品
-                if  it ~= item and it_id == jiejin_id then
+                if  it ~= item and it_id == JIEJIN_ID then
                     cj.SetItemCharges(it,cj.GetItemCharges(it)+1)
                     cj.RemoveItem(item)
+                    item = it
                 end
             end
         else
             -- body
         end
         
+        synthetic_item(unit,item)
     end)
 end
 
