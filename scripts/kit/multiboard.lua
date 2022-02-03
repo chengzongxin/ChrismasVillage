597365b4b13ee879c9c423708208852a84dca66d
USER-20210815FL\Administrator <8876142@qq.com>
2022-02-03 20:22:42 +0800

    多面板优化

--- scripts/kit/multiboard.lua
+++ scripts/kit/multiboard.lua
@@ -6,24 +6,44 @@ MULTIBOARD = function ()
     local mb = cj.CreateMultiboard()
     multiboard.mb = mb
 
-    local titles = {"玩家","杀敌","战力","金币","等级"}
+    local titles = {"玩家","杀敌","战力"}
+    local icons = {
+        "ReplaceableTextures\\CommandButtonsDisabled\\DISicon_pas_Holy_HolyGuidance.blp",
+        "ReplaceableTextures\\CommandButtonsDisabled\\DISicon_pas_Holy_WeaponMastery.blp",
+        "ReplaceableTextures\\CommandButtonsDisabled\\DISicon_pas_Holy_AvengersShield.blp"
+    }
+
+    local playerIcons = {
+        "ReplaceableTextures\\PassiveButtons\\PASBTNElunesBlessing.blp",
+        "ReplaceableTextures\\CommandButtons\\BTNDaggerOfEscape.blp",
+        "ReplaceableTextures\\CommandButtons\\BTNSpiritWalkerMasterTraining.blp"
+    }
 
     cj.MultiboardSetTitleText(mb,"Chirsmas Village")
     cj.MultiboardSetColumnCount(mb,#titles)
     cj.MultiboardSetRowCount(mb,#PlayerGroup+1)
-    cj.MultiboardSetItemsStyle(mb,true,false)
+    cj.MultiboardSetItemsStyle(mb,true,true) -- 显示文字，图标
     cj.MultiboardSetItemsValue(mb,"--")
-    cj.MultiboardSetItemWidth(mb,15/140)
+    cj.MultiboardSetItemsWidth(mb,0.05) -- 占屏幕宽度 5%
     cj.MultiboardSetItemsValueColor(mb,0,255,255,1)
     cj.MultiboardDisplay(mb,true)
 
-    for i=1,#titles do
-        -- 数组遍历索引从1开始，多面板是从{0,0}开始，需要统一协调
-        local item = cj.MultiboardGetItem(mb, 0, i-1)
-        cj.MultiboardSetItemValue(item, titles[i])
-        cj.MultiboardSetItemValueColor(item,255,255,255,1)
+    for i = 0, #PlayerGroup do
+        -- 设置第一行图标文字，多面板是从{0,0}开始
+        for j = 0, #titles - 1 do
+            local item = cj.MultiboardGetItem(mb, i, j)
+            if i == 0 then
+                -- 设置第一行标题
+                cj.MultiboardSetItemValue(item, titles[j+1])
+                cj.MultiboardSetItemValueColor(item,255,255,0,1)
+                cj.MultiboardSetItemIcon(item,icons[j+1])
+            else
+                -- 设置玩家图标
+                cj.MultiboardSetItemIcon(item,playerIcons[j+1])
+            end
+        end
     end
-    
+
     CREATE_MULTIBOARD_TIMER()
 end
 
@@ -41,7 +61,7 @@ REFRESH_MULTIBOARD = function ()
         local p = PlayerGroup[i]
         -- name
         local item = cj.MultiboardGetItem(mb, i,0)
-        cj.MultiboardSetItemValue(item, PLAYERS[p].name)
+        cj.MultiboardSetItemValue(item, PLAYERS[p].hero.name)
         -- kill
         local item = cj.MultiboardGetItem(mb, i,1)
         cj.MultiboardSetItemValue(item, string.format('%d',PLAYERS[p].kill))
